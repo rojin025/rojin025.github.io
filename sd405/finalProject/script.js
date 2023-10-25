146d7d9a33bb942ae3aa5bf9109e38175b926f6c
Rojin Bijukchhe <35796647+rojin025@users.noreply.github.com>
2023-10-25 17:49:35 -0500

    Update script.js
    Updating 
    - Adding animation
    - Adding sound interaction
    - changing design elements

--- sd405/finalProject/script.js
+++ sd405/finalProject/script.js
@@ -2,6 +2,8 @@ function init() {
     let secretNumber = randomNum();
     let score = 20;
     let highscore = 0;
+    let timer; 
+    let number = document.querySelector('.number');
 
 
     const displayMessage = function (message) {
@@ -11,31 +13,69 @@ function init() {
     function randomNum (){
         return(Math.floor(Math.random() * 20) + 1);
     }
+    function startTimer() {  //added
+        let seconds = 30;
+
+        function countdown() {
+            if (seconds >= 0) {
+                document.querySelector('.timer').textContent = seconds;
+                seconds--;
+               
+            } else {
+                displayMessage('Time\'s up! Better luck next time.');
+                document.querySelector('.score').textContent = 0;
+                document.querySelector('.guess').disabled = true;
+                clearInterval(timer)
+            }
+        }
+
+        countdown();
+        timer = setInterval(countdown, 600);
+    }
+
+    function stopTimer() {
+        clearInterval(timer);
+    }
+   
+    function wrongAnimation(){
+        number.classList.add('wrongAnime');
+        number.classList.add('wrongAnime');
+        setTimeout(function (){
+            number.classList.remove('wrongAnime');
+        },200)
+    }
+    
 
     document.querySelector('.check').addEventListener('click', function () {
         const guess = Number(document.querySelector('.guess').value);
         console.log(guess, typeof guess);
         console.log("secretNumber is " + secretNumber);
+        if (!timer) {
+            startTimer(); 
+        }
 
-        // When there is no input
-        if (!guess) {
-            displayMessage('Invalid Number!');
-
-            // When player wins
-        } else if (guess === secretNumber) {
+        
+        if (guess < 0 || guess > 20) {
+            displayMessage('Invalid input. Please enter a number between 1 and 20.');
+            return;  // i added to limit our entry
+       
+              // When player wins
+         } 
+         else if (guess === secretNumber) {
             displayMessage('Correct Number!');
-            document.querySelector('.number').textContent = secretNumber;
-
+            number.textContent = secretNumber;
             document.querySelector('body').style.backgroundColor = '#60b347';
-            document.querySelector('.number').style.width = '30rem';
-
+            number.style.width = '30rem';
+            var correctSound = document.getElementById('correct-sound');
+            correctSound.play();
             if (score > highscore) {
                 highscore = score;
                 document.querySelector('.highscore').textContent = highscore;
             }
-
+           stopTimer();
             // When guess is wrong
         } else if (guess !== secretNumber) {
+            wrongAnimation();
             if (score > 1) {
                 displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
                 score--;
@@ -44,8 +84,19 @@ function init() {
                 displayMessage('Better Luck Next Time!');
                 document.querySelector('.score').textContent = 0;
             }
+           
+            
+             var incorrectSound = document.getElementById('incorrect-sound');
+            incorrectSound.play();
         }
-
+       
+        if (typeof guess === 'number' ) {
+            const guessHistory = document.querySelector('.guess-history');
+            const listItem = document.createElement('li');
+            listItem.innerText = 'Guess #' + score + ': ' + guess;
+            guessHistory.appendChild(listItem);
+        }
+        
     });
 
     document.querySelector('.again').addEventListener('click', function () {
@@ -53,12 +104,32 @@ function init() {
         secretNumber = randomNum();
         displayMessage('Start guessing...');
         document.querySelector('.score').textContent = score;
-        document.querySelector('.number').textContent = '?';
+        number.textContent = '?';
         document.querySelector('.guess').value = '';
 
         document.querySelector('body').style.backgroundColor = '#222';
-        document.querySelector('.number').style.width = '15rem';
+        number.style.width = '15rem';
+        document.querySelector('.guess').disabled = false;
+       
+        document.querySelector('.timer').innerHTML=20;
+        startTimer(20);
     });
+   
+
+    document.querySelector('.sound-toggle').addEventListener('click', function() {
+        isSoundOn = !isSoundOn;
+        const bgMusic = document.getElementById('bg-music');
+        if (isSoundOn) {
+            bgMusic.play();
+        } else {
+            bgMusic.pause();
+        }
+    });
+
+    const bgMusic = document.getElementById('bg-music');
+    bgMusic.volume = 0.2; 
+    bgMusic.play();
+    
 
 }
 
