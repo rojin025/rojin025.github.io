9240ce00667a79c77b2b4868bddb0f5458d7ac97
Rojin Bijukchhe <35796647+rojin025@users.noreply.github.com>
2023-10-30 21:01:22 -0500

    Add files via upload

---
+++ SD415/Week1/day1.js
@@ -0,0 +1,42 @@
+// import promptsync from "promptsync"
+
+// const prompt = require("prompt-sync")();
+    
+// const tempInCelsius = prompt("Enter value in celsius: ")
+// const tempInFahrenheit = 9 / 5 * Number(tempInCelsius) + 32;
+// console.log(tempInFahrenheit);
+
+// let age = (0 || 10); // 10
+// let age2 = (0 ?? 10); // 0
+
+// let text = "";
+
+// for (let i = 1; i <=5 ; i++) {
+//     for (let j = 1; j <=5 ; j++) {
+//         text =  text.concat(i); 
+//     }
+//     console.log(text)
+//     text = "";    
+// }
+
+// function parseNumber() {
+// 	// let string = 'objects 10.6';
+// 	let string = 'a 10.6';
+// 	// let string = '10.6 objects';
+
+// 	let number1 = parseInt(string); //10
+// 	let number3 = parseFloat(string); //
+// 	let number2 = Number(string);   //10.6
+// 	let number4 = +string;  //10.6
+
+// 	console.log("Output of parseInt is: " + number1);
+// 	console.log("Output of parseFloat is: " + number3);
+// 	console.log("Output of Number is: " + number2);
+// 	console.log("Output of +'' is: " + number4);
+// }
+
+// parseNumber();
+
+
+
+
