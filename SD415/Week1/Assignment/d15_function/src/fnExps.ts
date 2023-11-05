98b9130fd0ddbf2658c132c68b88865db15c558b
Rojin Bijukchhe <35796647+rojin025@users.noreply.github.com>
2023-11-05 10:30:46 -0600

    Uploading Function Assignment
    I am uploading all the Function Expressions Assignments.

---
+++ SD415/Week1/Assignment/d15_function/src/fnExps.ts
@@ -0,0 +1,44 @@
+/**
+ *@returns {number} double the input
+ */
+export function double(num: number): number {
+  return num * 2;
+}
+
+/**
+ * @returns {number} 100 times the input
+ */
+export function times100(num: number): number {
+  return num * 100;
+}
+
+/**
+ * @returns {number} triple the input
+ */
+export function triples(num: number): number {
+  return num * 3;
+}
+
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+export function myMap(
+  arr: number[],
+  myFunction: (item: number) => number
+): number[] {
+  const result: number[] = [];
+  for (const item of arr) {
+    result.push(myFunction(item));
+  }
+  return result;
+}
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+export function myMapArrow(
+  arr: number[],
+  myFunction: (item: number) => number
+): number[] {
+  const result: number[] = arr.map((item) => myFunction(item));
+  return result;
+}
