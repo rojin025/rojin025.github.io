4a03a832c819a50664ceed5f12b7b43cc45cf0c8
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-11-08 14:13:37 -0600

    removing dist from .gitignore

---
+++ SD415/Week1/Assignment/d15_function/dist/src/app.js
@@ -0,0 +1,37 @@
+/**
+ *@returns {number} double the input
+ */
+export function double(num) {
+  return num * 2;
+}
+//test......
+/**
+ * @returns {number} 100 times the input
+ */
+export function times100(num) {
+  return num * 100;
+}
+/**
+ * @returns {number} triple the input
+ */
+export function triples(num) {
+  return num * 3;
+}
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+export function myMap(arr, myFunction) {
+  const result = [];
+  for (const item of arr) {
+    result.push(myFunction(item));
+  }
+  return result;
+}
+/**
+ * @returns {Array} creates a new array with function mapped to each element
+ */
+export function myMapArrow(arr, myFunction) {
+  const result = arr.map((item) => myFunction(item));
+  return result;
+}
+console.log("I am running ..........");
