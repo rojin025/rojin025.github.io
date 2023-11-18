4f36ae63776972e9016d2899a04744f47a80c993
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-11-18 17:58:06 -0600

    Updating Index & CSS

--- SD415/Week3/Assignment/d35recursion/dist/src/app.js
+++ SD415/Week3/Assignment/d35recursion/dist/src/app.js
@@ -1,24 +1,23 @@
-import {
-  //   sumTo,
-  //   factorial,
-  //   fibonacci,
-  //   outputList,
-  //   outputListLoop,
-  // reverseList,
-  reverseListLoop,
-} from "../src/recursion.js";
+import { 
+//   sumTo,
+//   factorial,
+//   fibonacci,
+//   outputList,
+//   outputListLoop,
+// reverseList,
+reverseListLoop, } from "../src/recursion.js";
 let list;
 list = {
-  value: 1,
-  next: {
-    value: 2,
+    value: 1,
     next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null,
-      },
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null,
+            },
+        },
     },
-  },
 };
 console.log(reverseListLoop(list));
--- SD415/Week3/Assignment/d35recursion/dist/src/recursion.js
+++ SD415/Week3/Assignment/d35recursion/dist/src/recursion.js
@@ -1,74 +1,73 @@
-export {
-  sumTo,
-  factorial,
-  fibonacci,
-  outputList,
-  outputListLoop,
-  reverseList,
-  reverseListLoop,
-  sumTreeValues,
-};
+export { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop, sumTreeValues, };
 function sumTo(n) {
-  if (n == 1) return 1;
-  return n + sumTo(n - 1);
+    if (n == 1)
+        return 1;
+    return n + sumTo(n - 1);
 }
 function factorial(n) {
-  if (n <= 1) return 1;
-  return n * factorial(n - 1);
+    if (n <= 1)
+        return 1;
+    return n * factorial(n - 1);
 }
 function fibonacci(n) {
-  if (n < 0) return 0;
-  else if (n <= 1) return n;
-  else return fibonacci(n - 2) + fibonacci(n - 1);
+    if (n < 0)
+        return 0;
+    else if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 2) + fibonacci(n - 1);
 }
 function outputList(node) {
-  let str = "";
-  outputListRecursive(node);
-  return str;
-  function outputListRecursive(list) {
-    if (list) {
-      str += list.value;
-      if (list.next) outputListRecursive(list.next);
+    let str = "";
+    outputListRecursive(node);
+    return str;
+    function outputListRecursive(list) {
+        if (list) {
+            str += list.value;
+            if (list.next)
+                outputListRecursive(list.next);
+        }
     }
-  }
 }
 function outputListLoop(node) {
-  let result = "";
-  outputListLoopIterative(node);
-  return result;
-  function outputListLoopIterative(list) {
-    while (list) {
-      console.log(list.value);
-      result += list.value;
-      list = list.next;
+    let result = "";
+    outputListLoopIterative(node);
+    return result;
+    function outputListLoopIterative(list) {
+        while (list) {
+            console.log(list.value);
+            result += list.value;
+            list = list.next;
+        }
     }
-  }
 }
 function reverseList(node) {
-  let result = "";
-  reverseListRecursive(node);
-  return result;
-  function reverseListRecursive(list) {
-    if (list) {
-      if (list.next) reverseListRecursive(list.next);
-      result += list.value;
+    let result = "";
+    reverseListRecursive(node);
+    return result;
+    function reverseListRecursive(list) {
+        if (list) {
+            if (list.next)
+                reverseListRecursive(list.next);
+            result += list.value;
+        }
     }
-  }
 }
 function reverseListLoop(node) {
-  let result = "";
-  innerFun(node);
-  return result;
-  function innerFun(list) {
-    if (list.next) {
-      innerFun(list.next);
+    let result = "";
+    innerFun(node);
+    return result;
+    function innerFun(list) {
+        if (list.next) {
+            innerFun(list.next);
+        }
+        result += list.value;
     }
-    result += list.value;
-  }
 }
 function sumTreeValues(treeNode) {
-  if (treeNode === null) return 0;
-  const calLeftBranch = sumTreeValues(treeNode.left);
-  const callRightBranch = sumTreeValues(treeNode.right);
-  return treeNode.value + calLeftBranch + callRightBranch;
+    if (treeNode === null)
+        return 0;
+    const calLeftBranch = sumTreeValues(treeNode.left);
+    const callRightBranch = sumTreeValues(treeNode.right);
+    return treeNode.value + calLeftBranch + callRightBranch;
 }
