e58c6821b022fa1baa241222e6a3652466b9d97f
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-11-26 09:43:20 -0600

    practising

--- SD500/app.ts
+++ SD500/app.ts
@@ -1,2 +1,12 @@
 // Async
 
+//Stack
+// JS work at one at time - since it is happening so fast. It looks multitasking
+
+console.log("Sending request to server");
+setTimeout(()=>{
+    console.log("Data received after 2 sec.");
+    console.log("Data receiving back...");
+}, 2000);
+
+
