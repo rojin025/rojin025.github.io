d12fbe3f3d5ce640badbfe3a8d993c9d11475b4e
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-12-03 16:50:34 -0600

    Update Std

--- SD500/Classwork/exam.ts
+++ SD500/Classwork/exam.ts
@@ -47,8 +47,30 @@ function getStudent_V2(student_id: number): Student | null {
     if (!stdExist) return null;
     
     return {id: student_id, ...structuredClone(data_2[student_id])};
-    
 }
+
+function updateStudent_V1(updateStudent: Student): boolean{
+    if(!getStudent_V1(updateStudent.id)) return false;
+
+    const updatedData = data_1.map( std => {   
+        if(std.id === updateStudent.id ) return updateStudent;
+        return std;
+    });
+    data_1 = updatedData;
+    return true;
+}
+
+function updateStudent_V2(updateStudent: Student): boolean {
+    if(!getStudent_V2(updateStudent.id)) return false;
+    
+    console.log("UPDATING");
+    const {id, name, courses} = updateStudent
+    const updatedData = structuredClone(data_2);
+    updatedData[id] = { name , courses};
+    data_2 = updatedData;
+    return true;
+}
+
 
 // Adding 
 addStudent_V2({
@@ -73,15 +95,43 @@ addStudent_V1({
 });
 
 // GetStd test
-console.log("Getting Std with ID - 1 ",getStudent_V1(1));
-console.log("Getting Std with ID - 10 ",getStudent_V1(10));
+// console.log("Getting Std with ID - 1 ",getStudent_V1(1));
+// console.log("Getting Std with ID - 10 ",getStudent_V1(10));
 
-console.log("Getting Std with ID - 5 ",getStudent_V2(5));
-console.log("Getting Std with ID - 10 ",getStudent_V2(10));
+// console.log("Getting Std with ID - 5 ",getStudent_V2(5));
+// console.log("Getting Std with ID - 10 ",getStudent_V2(10));
 
 console.log(data_1);
 console.log("--------------------------------------------------------");
 console.log(data_2);
+
+// // Update
+// updateStudent_V1({
+//     id : 1,
+//     name: "Zeus",
+//     courses: [],
+// });
+
+// console.log(updateStudent_V1({
+//     id : 100,
+//     name: "Zeus",
+//     courses: [],
+// }));
+
+updateStudent_V2({
+    id : 5,
+    name: "GOD",
+    courses: [],
+});
+
+console.log(updateStudent_V2({
+    id : 100,
+    name: "Zeus",
+    courses: [],
+}));
+
+console.log("Updated Data: ",data_1);
+console.log("Updated Data: ",data_2);
 
 // const stdObj = {
 //     '4': { name: 'Rojin', course: [{id:1, title:"WAP",description: "wap-500" }] },
