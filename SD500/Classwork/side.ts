a724a0fcd5c077bfc86ae371d020c6361ae2ec13
Rojin Bijukchhe <ro_jin25@icloud.com>
2023-12-10 17:04:02 -0600

    Working on my Promises

--- SD500/Classwork/side.ts
+++ SD500/Classwork/side.ts
@@ -1,10 +1,46 @@
+// // class Player {
+// //     readonly firstName: string;
+// //     readonly lastName: string;
+// //     private score = 0;
+// //     constructor(firstName: string, lastName: string){
+// //         this.firstName = firstName;
+// //         this.lastName = lastName;
+// //     }
+
+// //     private secretMethod(): void {
+// //         console.log("The Secret!");
+// //     }
+// // }
+
+// // Short Cut
+
+// interface Paycheck {
+//     salary: number;
+// }
+
+// interface CheckPlayer {
+//     check(): void;
+// }
+
 // class Player {
-//     readonly firstName: string;
-//     readonly lastName: string;
-//     private score = 0;
-//     constructor(firstName: string, lastName: string){
-//         this.firstName = firstName;
-//         this.lastName = lastName;
+//     protected _score = 0;
+//     private _life = 10;
+//     constructor(
+//         public firstName: string,
+//         public lastName: string
+//     ) {}
+
+//     get fullName(): string {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+
+//     get score(){
+//         return this._score;
+//     }
+
+//     set score(newScore: number){
+//         if(newScore<0) throw new Error ("Score cannot be negative!") ;
+//         this._score = newScore;
 //     }
 
 //     private secretMethod(): void {
@@ -12,154 +48,121 @@
 //     }
 // }
 
-// Short Cut
+// class Admin extends Player implements Paycheck, CheckPlayer {
+//     constructor(public firstName: string , public lastName: string, public salary: number){
+//         super(firstName,lastName);
+//     }
+//     maxScore(score: number){
+//         this._score = score;
+//     }
+//     check(){
+//         console.log("Checking Players!");
+//     }
+// }
 
-interface Paycheck {
-    salary: number;
-}
+// const player1 = new Player("Player", "One");
+// console.log("Player Full Name: ", player1.fullName);
+// console.log("Player Score: ", player1.score);
+// // player1.secretMethod();  Can not access
 
-interface CheckPlayer {
-    check(): void;
-}
+// player1.score = 10;
+// console.log("Updated Score: ", player1.score);
 
-class Player {
-    protected _score = 0;
-    private _life = 10;
-    constructor(
-        public firstName: string,
-        public lastName: string
-    ) {}
+// const myAdmin = new Admin("Super", "Man", 10000);
+// myAdmin.maxScore(90);
+// console.log(myAdmin);
+// myAdmin.check();
 
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
+// // console.log(" ####################################################################");
+// // Abstract Class
+// abstract class Employee {
+//     constructor(public first: string, public last: string){}
+//     abstract getPay(salary: number): number;
+//     sayHi(){
+//         console.log("Hello World!");
+//     }
+// }
 
-    get score(){
-        return this._score;
-    }
+// class FullTimeEmp extends Employee {
+//     constructor(
+//         first: string,
+//         last: string,
+//         private yearlySalary: number
+//         ){
+//         super(first, last);
+//     }
+//     getPay(): number {
+//         return this.yearlySalary;
+//     }
+//     multi(x: number, y: number): number{
+//         return x * y;
+//     }
+// }
 
-    set score(newScore: number){
-        if(newScore<0) throw new Error ("Score cannot be negative!") ;
-        this._score = newScore;
-    }
+// class PartTimeEmp extends Employee{
+//     constructor(
+//         first: string,
+//         last: string,
+//         public hrRate: number,
+//         public workingHour: number
+//     ){
+//         super(first, last);
+//     }
+//     getPay(): number {
+//         return this.hrRate * this.workingHour;
+//     }
+//     pay = () => {this.hrRate * this.workingHour };
+// }
 
-    private secretMethod(): void {
-        console.log("The Secret!");
-    }
-}
+// const ram = new FullTimeEmp("Ram", "Baral", 135000);
+// // console.log(ram);
+// // console.log("Pay Ram: ",ram.getPay());
 
-class Admin extends Player implements Paycheck, CheckPlayer {
-    constructor(public firstName: string , public lastName: string, public salary: number){
-        super(firstName,lastName);
-    }
-    maxScore(score: number){
-        this._score = score;
-    }
-    check(){
-        console.log("Checking Players!");
-    }
-}
-
-const player1 = new Player("Player", "One");
-console.log("Player Full Name: ", player1.fullName);
-console.log("Player Score: ", player1.score);
-// player1.secretMethod();  Can not access
-
-player1.score = 10;
-console.log("Updated Score: ", player1.score);
-
-const myAdmin = new Admin("Super", "Man", 10000);
-myAdmin.maxScore(90);
-console.log(myAdmin);
-myAdmin.check();
+// const shyam = new PartTimeEmp("Shyam" , "Grung", 30, 2000);
+// // console.log("Shyam: " , shyam);
+// // console.log("Pay Shyam ", shyam.getPay());
 
 // console.log(" ####################################################################");
-// Abstract Class 
-abstract class Employee {
-    constructor(public first: string, public last: string){}
-    abstract getPay(salary: number): number;
-    sayHi(){
-        console.log("Hello World!");
-    }
-}
+// // // Bind
+// //     //  The bind method will clone the function and permanently replace this
+// //     //  inside the function with the provided parameter.
+// //     //  will permanently attach this to var myGetPay
+// // const myGetPay = shyam.getPay.bind(shyam);
+// // console.log("Bind Method : Shyam Salary ", myGetPay());
+// // console.log("------ Function Currying ------");
+// // const mutiplie2 = ram.multi.bind(null, 2); // here x will be cloned to be 3;
+// // const square3 = ram.multi.bind({},3,3);   // we are multiplying 3 with 3
 
-class FullTimeEmp extends Employee {
-    constructor(
-        first: string, 
-        last: string,
-        private yearlySalary: number
-        ){
-        super(first, last);
-    }
-    getPay(): number {
-        return this.yearlySalary;
-    }
-    multi(x: number, y: number): number{
-        return x * y;
-    }
-}
+// // console.log("Muliple of 2 is ", mutiplie2(2));
+// // console.log("Square of 3 is ", square3());
 
-class PartTimeEmp extends Employee{
-    constructor(
-        first: string, 
-        last: string,
-        public hrRate: number, 
-        public workingHour: number
-    ){
-        super(first, last);
-    }
-    getPay(): number {
-        return this.hrRate * this.workingHour;
-    }
-    pay = () => {this.hrRate * this.workingHour };
-}
+// console.log("------ Call and Apply ------");
+// // Call and Apply
+//     // Does not clone the original function
+//     // it is only used to invoke the funtion with (Parameter)
+//     // a for array
 
-const ram = new FullTimeEmp("Ram", "Baral", 135000);
-// console.log(ram);
-// console.log("Pay Ram: ",ram.getPay());
+// // console.log("Call() ");
+// // console.log(ram.getPay());
 
-const shyam = new PartTimeEmp("Shyam" , "Grung", 30, 2000);
-// console.log("Shyam: " , shyam);
-// console.log("Pay Shyam ", shyam.getPay());
+// const person = { name: "Asaad",
+// printNow() { console.log(`Hi ${this.name}!`); } };
+// // person.printNow(); // Hi Asaad!
+// // person.printNow.apply(person); // Hi Asaad!
 
-console.log(" ####################################################################");
-// // Bind
-//     //  The bind method will clone the function and permanently replace this
-//     //  inside the function with the provided parameter.
-//     //  will permanently attach this to var myGetPay
-// const myGetPay = shyam.getPay.bind(shyam);
-// console.log("Bind Method : Shyam Salary ", myGetPay());
-// console.log("------ Function Currying ------");
-// const mutiplie2 = ram.multi.bind(null, 2); // here x will be cloned to be 3;
-// const square3 = ram.multi.bind({},3,3);   // we are multiplying 3 with 3
+// // Function borrowing
+// const ramu ={ name: "Ramu"};
+// person.printNow.apply(ramu);    // Hi Ramu! // From person
 
-// console.log("Muliple of 2 is ", mutiplie2(2));
-// console.log("Square of 3 is ", square3());
+// // const myPay = shyam.pay.call(shyam);
+// // console.log("Arrow: ", myPay);    // still undefined
+// // console.log("End!!");
 
-console.log("------ Call and Apply ------");
-// Call and Apply
-    // Does not clone the original function
-    // it is only used to invoke the funtion with (Parameter)
-    // a for array
+// // Arrow function
+//     // "this" is evaluated to its surrounding scope of the arrow
+//     // Does not care how it is invoked or from where?
 
-// console.log("Call() ");
-// console.log(ram.getPay());
-
-const person = { name: "Asaad",
-printNow() { console.log(`Hi ${this.name}!`); } };
-// person.printNow(); // Hi Asaad!
-// person.printNow.apply(person); // Hi Asaad!
-
-// Function borrowing
-const ramu ={ name: "Ramu"};
-person.printNow.apply(ramu);    // Hi Ramu! // From person
-
-// const myPay = shyam.pay.call(shyam);
-// console.log("Arrow: ", myPay);    // still undefined
-// console.log("End!!");   
-
-
-
-// Arrow function
-    // "this" is evaluated to its surrounding scope of the arrow
-    // Does not care how it is invoked or from where?   
+console.log(
+  " ####################################################################"
+);
+// 2nd
